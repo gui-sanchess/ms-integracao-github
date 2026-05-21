07c659b01b169f3f5c6f9a54d6ddc1b014de6e4e
Guilherme Sanches <10436672@mackenzista.com.br>
2026-05-21 15:13:52 -0300

    adicionando usuario_id e url_documento na entidade

--- domain/entities.py
+++ domain/entities.py
@@ -7,8 +7,10 @@ class Artefato:
     nome_arquivo: str
     conteudo_extraido: str
     projeto_id: int
+    usuario_id: int  # <--- O atributo que faltava para a integração não quebrar!
+    url_documento: Optional[str] = None  # <--- Adicionado para ficar igual ao Upload
     tipo_classificado: Optional[str] = "Desconhecido"
     tags: List[str] = field(default_factory=list)
     resumo: Optional[str] = "Sem resumo."
-    data_upload: datetime = field(default_factory=datetime.now)
+    data_upload: datetime = field(default_factory=datetime.utcnow) # utcnow evita conflitos de fuso horário
     id: Optional[int] = None
